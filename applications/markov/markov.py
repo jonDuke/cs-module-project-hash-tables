69b18958be57f394cb6130a87c9af301631cd955
jonDuke <jonduke90@gmail.com>
2020-08-05 13:06:46 -0700

    completed markov generator

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,26 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+words = words.split()  # change to a list of words
+word_table = {}
+
+# for all but the last word...
+for i in range(len(words)-1):
+    # add the word after it to the table
+    if words[i] in word_table:
+        word_table[words[i]].append(words[i+1])
+    else:
+        word_table[words[i]] = [words[i+1]]
 
 
 # TODO: construct 5 random sentences
-# Your code here
+for _ in range(5):
+    word = random.choice(list(word_table.keys()))
+    sentence = word + " "
 
+    # while the last word added does not end with punctuation
+    while word[-1] not in ".?!":
+        word = random.choice(word_table[word])  # get next word randomly
+        sentence += word + " "  # add to the sentence
+
+    print(sentence, '\n')
