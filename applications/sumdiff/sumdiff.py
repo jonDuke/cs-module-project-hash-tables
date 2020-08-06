e9ceff7a9e125e0a8e750f0f0d5b2dea18c14e91
jonDuke <jonduke90@gmail.com>
2020-08-06 13:57:11 -0700

    completed sum-diff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -11,5 +11,17 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
-# Your code here
+# cache answers to f(x)
+cache = {}
+for n in q:
+    cache[n] = f(n)
 
+# check all combinations of a,b,c,d
+for a in q:
+    for b in q:
+        for c in q:
+            for d in q:
+                # check the condition
+                if cache[a] + cache[b] == cache[c] - cache[d]:
+                    # print this combination
+                    print(f"f({a}) + f({b}) = f({c}) - f({d})    {cache[a]} + {cache[b]} = {cache[c]} - {cache[d]}")
