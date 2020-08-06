e85cb6503ca80a6e6f53dc934ca3f4bec136f59f
jonDuke <jonduke90@gmail.com>
2020-08-06 13:16:50 -0700

    completed expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,20 @@
-# Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    key = (x,y,z)
+    if key in cache:
+        return cache[key]  # return cached answer
+    else:
+        # calculate the sequence
+        if x <= 0:
+            cache[key] = y + z
+            return y + z
+        else:
+            value = (expensive_seq(x-1, y+1, z) +
+                     expensive_seq(x-2, y+2, z*2) +
+                     expensive_seq(x-3, y+3, z*3))
+            cache[key] = value
+            return value
 
 
 
